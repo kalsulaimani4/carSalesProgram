03064c2af39f13bd1727fb3cc9bd82db033cf983
khalil alsulaimani <alsulaimani.2000@outlook.com>
2020-04-28 21:34:50 +0300

    added all the needed vectors to the main to store all the needed information

--- src/CarSales.java
+++ src/CarSales.java
@@ -1,6 +1,15 @@
+import java.util.Vector;
+
 public class CarSales {
 
     public static void main(String[] args){
 
+        Vector<Integer> idVec= new Vector<>(50);
+        Vector<String> modelVec= new Vector<>(50);
+        Vector<String> vendorVec= new Vector<>(50);
+        Vector<Integer> yearVec= new Vector<>(50);
+        Vector<Float> priceVec= new Vector<>(50);
+        Vector<Part> partsVec= new Vector<>(50);
+        Vector<Boolean> usageVec= new Vector<>(50);
     }
 }
