1d38bf9a1d390ffe164c7a8f118b1829a268c434
khalil alsulaimani <alsulaimani.2000@outlook.com>
2020-04-28 21:45:46 +0300

    added files to git ignore and put all the switch cases of the program in a while loop

--- src/CarSales.java
+++ src/CarSales.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 import java.util.Vector;
 
 public class CarSales {
 
     public static void main(String[] args){
-
+        Scanner scan = new Scanner(System.in);
         Vector<Integer> idVec= new Vector<>(50);
         Vector<String> modelVec= new Vector<>(50);
         Vector<String> vendorVec= new Vector<>(50);
@@ -11,5 +12,29 @@ public class CarSales {
         Vector<Float> priceVec= new Vector<>(50);
         Vector<Part> partsVec= new Vector<>(50);
         Vector<Boolean> usageVec= new Vector<>(50);
+
+
+        int numOfTermination=0;
+        int index=0;
+        while(numOfTermination!=3){
+            System.out.println("1- Add new car "+"\n"+"2- Print all available  cars"+"\n"+"3- Exit");
+            System.out.println("enter number for the wanted action ");
+            int choice=scan.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Enter  ");
+
+                    continue;
+
+                case 2:
+                    continue;
+
+                case 3:
+                    numOfTermination=3;
+                    System.out.println("Program terminated ");
+                    continue;
+            }
+        }
     }
 }
