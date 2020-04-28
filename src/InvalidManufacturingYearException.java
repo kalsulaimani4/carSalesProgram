b6d4333944ec64e16ed5cc5be8ae6ff07be10e8e
khalil alsulaimani <alsulaimani.2000@outlook.com>
2020-04-28 21:15:49 +0300

    added a test method that throws InvalidManufacturingYearException and handled the exception with the given message from the exam

--- src/InvalidManufacturingYearException.java
+++ src/InvalidManufacturingYearException.java
@@ -1,7 +1,13 @@
 public class InvalidManufacturingYearException extends  Exception{
 
-    InvalidManufacturingYearException(String s){
+    public InvalidManufacturingYearException(String s){
         super(s);
     }
-    
+
+    public static void test(int year)throws InvalidManufacturingYearException{
+        if(year>2020 || year<1900){
+            throw new InvalidManufacturingYearException( "Manufacturing Year should be older than or equal to this year" );
+        }
+    }
+
 }
